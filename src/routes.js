4c69858c53f59257a35693d1f22545a064748b69
Henrique Rocha <henriquerochaserranoh@gmail.com>
2020-12-08 18:40:25 -0300

    init insta clone. configure: routes and status bar

---
+++ src/routes.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Image } from 'react-native'
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
+
+import Feed from './pages/Feed'
+
+import logo from './assets/instagram.png'
+
+const RootStack = createStackNavigator({
+  Feed
+}, {
+  defaultNavigationOptions: {
+    headerTitleAlign: 'center',
+    headerTitle: () => <Image source={logo} />,
+    headerStyle: {
+      backgroundColor: '#f5f5f5'
+    }
+  }
+})
+
+const Routes = createAppContainer(RootStack)
+
+export default Routes
